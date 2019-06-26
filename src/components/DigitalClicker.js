b87abf633d7d6b59e462483be612dbf1f6f4c8a2
github username <gregm9999@gmail.com>
2019-06-26 09:25:32 -0700

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,26 @@
-// Code DigitalClicker Component Here
+import React, { Component } from "react";
+
+class DigitalClicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timesClicked: 0
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => {
+      return { timesClicked: (prevState.timesClicked += 1) };
+    });
+  };
+
+  render() {
+    return (
+      <button type="button" onClick={this.handleClick}>
+        {this.state.timesClicked}
+      </button>
+    );
+  }
+}
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,61 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from "react";
+
+class YouTubeDebugger extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: "1080p"
+        }
+      }
+    };
+  }
+
+  handleBitrateChange = () => {
+    console.log(this);
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    });
+  };
+
+  handleResolutionChange = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.video,
+          resolution: "720p"
+        }
+      }
+    });
+  };
+  render() {
+    return (
+      <div>
+        <button
+          type="button"
+          className="bitrate"
+          onClick={this.handleBitrateChange}
+        >
+          Change Bitrate
+        </button>
+        <button
+          type="button"
+          className="resolution"
+          onClick={this.handleResolutionChange}
+        >
+          Change Resolution
+        </button>
+      </div>
+    );
+  }
+}
+
+export default YouTubeDebugger;
