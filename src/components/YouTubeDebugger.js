0bcb61f2cc08773a56aa138a30a2a331550b823f
Bureaucratix <bureaucratix@gmail.com>
2019-06-28 12:27:21 -0700

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,46 @@
 // Code YouTubeDebugger Component Here
+
+
+import React, { Component } from 'react';
+
+class CLASSNAME extends Component {
+    constructor() {
+        super();
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+          };
+    }
+
+    handleBitrate = () => {
+        this.setState({
+            settings: {...this.state.settings,
+            bitrate: 12
+            }
+        })
+    }
+    handleResolution = () => {
+        this.setState({
+            settings: {...this.state.settings,
+                video: {...this.state.settings.video,
+            resolution: '720p'
+            }
+        }
+        })
+    }
+
+    render() {
+        return (<div>
+            <button className='bitrate' onClick={this.handleBitrate}>Change Bitrate to 12</button>
+            <button className='resolution' onClick={this.handleResolution}>Change resolution to 720p</button>
+        </div>)
+    }
+}
+
+export default CLASSNAME;
